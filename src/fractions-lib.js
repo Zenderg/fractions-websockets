d495670fb293867508a434f36e409887182b8036
dm.kuzin <danil54658798@gmail.com>
2019-06-27 10:52:41 +0300

    new several functions for lib

--- src/fractions-lib.js
+++ src/fractions-lib.js
@@ -1,3 +1,5 @@
+export const make = (num, denom) => [num, denom];
+
 export const num = arr => arr[0];
 
 export const denom = arr => arr[1];
@@ -27,3 +29,59 @@ export const actions = {
   '*': mul,
   '/': div
 };
+
+export const gcd = fract => {
+  const newNum = num(fract);
+  const newDenom = denom(fract);
+
+  if (!newDenom) return newNum;
+
+  return gcd(make(newDenom, newNum % newDenom));
+};
+
+export const scaleFract = fract => {
+  let gcdFract = gcd(fract);
+
+  return make(num(fract) / gcdFract, denom(fract) / gcdFract);
+};
+
+export const doAction = (arr, i) => {
+  const newArr = arr;
+  const prev = newArr[i - 1];
+  const next = newArr[i + 1];
+  const val = scaleFract(actions[newArr[i]](prev, next));
+
+  newArr.splice(i - 1, 3, val);
+
+  return newArr;
+};
+
+export const removeHighPriority = arr => {
+  let result = arr;
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === '*' || result[i] === '/') {
+      result = doAction(result, i);
+      i--;
+    }
+  }
+
+  return result;
+};
+
+const countUp = arr => {
+  let newArr = removeHighPriority(arr);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === '+' || newArr[i] === '-') {
+      newArr = doAction(newArr, i);
+      i--;
+    }
+  }
+
+  const result = scaleFract(newArr[0]);
+
+  return result;
+};
+
+export default countUp;
